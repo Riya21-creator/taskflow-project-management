1cc1e84f4986e732e2abd220f0455723cda05e80
Riya Mishra <2k22.cse.2213354@gmail.com>
2026-05-02 14:30:51 +0530

    fix navbar spacing properly

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-
-const Navbar = () => {
+import "../index.css";
+  const Navbar = () => {
   const { user, logoutUser } = useAuth();
   const navigate = useNavigate();
 
@@ -16,21 +16,22 @@ const Navbar = () => {
     <nav className="navbar">
       <h2>TaskFlow</h2>
 
-      <div>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/my-tasks">My Tasks</Link>
+    <div className="nav-links">
+  <Link to="/dashboard">Dashboard</Link>
+  <Link to="/projects">Projects</Link>
+  <Link to="/my-tasks">My Tasks</Link>
 
-        {user.role === "Admin" && (
-          <>
-            <Link to="/create-project">Create Project</Link>
-            <Link to="/create-task">Create Task</Link>
-          </>
-        )}
+  {user?.role === "Admin" && (
+    <>
+      <Link to="/create-project">Create Project</Link>
+      <Link to="/create-task">Create Task</Link>
+    </>
+  )}
 
-        <span className="role">{user.role}</span>
-        <button onClick={handleLogout}>Logout</button>
-      </div>
+  <span className="role">{user?.role}</span>
+
+  <button onClick={logoutUser}>Logout</button>
+</div>
     </nav>
   );
 };
